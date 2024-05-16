016be0186310231b1d5f04c045c1ae2de01245fe
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-16 21:07:58 +0200

    Corretto. Avevo assegnato una variabile a parola insensatamente perche pensavo non mi funsionasse lo scorrimento indice. Invece era per il return che era dentro il for. Corretto questo pomeriggio. Ora provavo a vedere se funzionava con l'argomento e giustamente funziona.

--- palidroma/js/script.js
+++ palidroma/js/script.js
@@ -14,10 +14,9 @@ if(userWord === userWordReverse){
 
 
 function palindromo (parola){
-    let word = parola
     let wordReverse = "";
-    for(let i = word.length - 1; i >= 0; i--){
-        wordReverse += word[i];
+    for(let i = parola.length - 1; i >= 0; i--){
+        wordReverse += parola[i];
     }
     return wordReverse
 }
